49113b5d6a65fb185b7ffc2322b9d847befe28fd
manu <email@example.com>
2025-01-21 21:39:30 +0100

    i'm trying

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,20 @@ function goToSlide(index) {
 window.addEventListener('load', () => {
   goToSlide(0);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadModeContent(0);
+  loadModeContent(1);
+  loadModeContent(2);
+  loadModeContent(3);
+  loadModeContent(4);
+});
+
+function loadModeContent(modeIndex) {
+  fetch(`src/mode${modeIndex + 1}.html`)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(`mode${modeIndex + 1}`).innerHTML = data;
+    })
+    .catch(error => console.error('Error loading mode content:', error));
+}
